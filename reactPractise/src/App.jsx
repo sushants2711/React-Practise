053d7ba43c3982c19537aec04e0a8fd756809c83
sushantkumarsingh <singhji1359@gmail.com>
2025-01-06 19:14:08 +0530

    Solve Question 3 with some animation spin and responsive

--- reactPractise/src/App.jsx
+++ reactPractise/src/App.jsx
@@ -3,19 +3,23 @@ import { movies } from "./API/All_Api"
 import { Question1 } from "./components/Question1"
 import { Question2 } from "./components/Question2"
 import { products } from "./API/All_Api"
+import { library } from "./API/All_Api"
+import { Question3 } from "./components/Question3"
+
 
 function App() {
   
   const [ movieData, setMovieData ] = useState(movies);
   const [ productData, setproductData ] = useState(products);
-
+  const [ book, setBook ] = useState(library)
  
 
 
   return (
     <>
     {/* <Question1 movie={movieData}/> */}
-    <Question2 product={productData} />
+    {/* <Question2 product={productData} /> */}
+    <Question3 books={book} />
     </>
   )
 }
---
+++ reactPractise/src/components/Question3.jsx
@@ -0,0 +1,35 @@
+export const Question3 = ({ books }) => {
+    return (
+        <>
+
+            <div className="max-w-full mx-auto shadow-lg">
+                <header>
+                    <h1 className="text-center text-2xl font-bold my-3 md:my-4 border-b-2 border-black first-letter:text-3xl font-serif ">Books Library</h1>
+                </header>
+            </div>
+            {
+                books.length >= 1 ? (
+                    <div className="max-w-[1320px] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 ">
+                        {
+                            books.map((currEle, index) => (
+                                <div key={index} className="text-center bg-cyan-100 shadow-lg h-[300px] w-[300px] rounded-full border-2 border-gray-300 flex flex-col justify-center items-center my-3 md:my-5 mx-3 md:mx-8 hover:border-b-2 hover:border-green-300 hover:animate-spin">
+                                    <h1 className="text-2xl font-bold hover:text-3xl duration-200 mt-4">{currEle.title}</h1>
+                                    <h4 className="text-xl font-semibold mt-4">{currEle.author}</h4>
+                                    <p className="font-semibold mt-4">{currEle.rating}*</p>
+                                </div>
+                            ))
+                        }
+
+                    </div>
+
+
+                ) : (
+                    <div className="max-w-[1320px] mx-auto text-center py-5">
+                        <p className="text-2xl font-semibold">No Books details available.</p>
+                    </div>
+
+                )
+            }
+        </>
+    )
+}
